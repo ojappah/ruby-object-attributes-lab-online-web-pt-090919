fd955ef448031d73ae6c71e3df67fa00301c8e78
generous-vector-9581 <ojappah@gmail.com>
2019-10-08 01:40:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,14 @@
 class Dog
-  def initialize (dogs_name, dogs_breed = "Mutt")
-    @name = dogs_name
-    @breed = dogs_breed
+  def name=(name)
+    @name=name
   end
-
-  def name=dogs_name
-    @name = dogs_name
-  end
-  
-  def breed=dogs_breed
-    @breed = dogs_breed
-  end
-
   def name
     @name
   end
+  def breed=(breed)
+    @breed=breed
+  end
   def breed
     @breed
-  end 
-
-end 
-  
+  end
+end
